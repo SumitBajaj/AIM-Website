9fc6a8539c6497ae79fa7310284ec5e5f6390d79
priyajoshi90 <priya.apr9@gmail.com>
2014-03-27 15:33:19 +0530

    opacity

--- web/scripts/opacity.js
+++ web/scripts/opacity.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
 	  var clickIndex = $(this).parent().index();
 	  $(".nav li").css('opacity', '1');
 	  $(".nav li:not(:nth-child("+(clickIndex+1)+"))").css('opacity', '0.5');
+	  $(".nav li:last-child").css('opacity', '1');
 	});
 });
