cdce9bc460e4864ac62a3a6a0bd6aa8fdb7590f6
unknown <Sumit Bajaj@GGN000079112006.ads.mckinsey.com>
2014-09-04 11:54:48 +0530

    changed the name to index

---
+++ web/Gruntfile.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            assets: {
+                files: 'app/assets/**/*',
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+}
